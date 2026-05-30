034fc2ed470a5e8cde2687b8337d0d40b24724d1
diegonascimento2023 <diego.oliveira24023@alunos.ufersa.edu.br>
2026-05-30 12:01:12 -0300

    chore: configura projeto maven com sqlite e estrutura de pastas

---
+++ src/main/java/com/barbearia/App.java
@@ -0,0 +1,13 @@
+package com.barbearia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
